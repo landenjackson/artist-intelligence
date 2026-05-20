3ac16bc84cf978fd20c06732139db81461d0c6ca
Landen Jackson <jacksonlanden3986@gmail.com>
2026-05-20 20:33:21 +0000

    Remove .next build artifacts from repo

--- .next/server/app/api/artist/[artist]/route.js
+++
@@ -1,6 +0,0 @@
-var R=require("../../../../chunks/[turbopack]_runtime.js")("server/app/api/artist/[artist]/route.js")
-R.c("server/chunks/[root-of-the-server]__0i2n66e._.js")
-R.c("server/chunks/[root-of-the-server]__0j8-xkl._.js")
-R.c("server/chunks/_next-internal_server_app_api_artist_[artist]_route_actions_0x1z.j7.js")
-R.m(7158)
-module.exports=R.m(7158).exports
--- .next/server/chunks/ssr/_next-internal_server_app_artist_[artist]_page_actions_08pq-0p.js
+++
@@ -1,3 +0,0 @@
-module.exports=[28142,(a,b,c)=>{}];
-
-//# sourceMappingURL=_next-internal_server_app_artist_%5Bartist%5D_page_actions_08pq-0p.js.map
